fdc85cf9704f0e383a0bf8bc5653215814b9b63f
Oleg Parubochyi <oleg.parubochyy@perfectial.com>
2017-08-17 17:34:21 +0300

    Initial commit

---
+++ SimpleConsoleApp/SimpleConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        const string version = "1.0";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Version {version}");
+        }
+    }
+}
